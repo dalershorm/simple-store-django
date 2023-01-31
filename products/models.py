a7002889c3f4bb595d1162a17851415039a67763
abdurahman <abdurahman@abdurahmans-MacBook-Pro.local>
2023-01-31 21:27:12 +0500

    add modes category and product

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     show_in_home = models.CharField(choices=STATUS_CHOICES, default='active')
 
     def slug(self):
-        return slugify(self.slug)
+        return slugify(self.name)
 
     def __str__(self):
         return self.name
@@ -27,14 +27,14 @@ class Product(models.Model):
 
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
-    price = models.DecimalField(default=1.0)
+    price = models.DecimalField()
     status = models.CharField(choices=STATUS_CHOICES, default='active')
     image = models.ImageField(upload_to='products/%Y/%m/%d')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def slug(self):
-        return slugify(self.slug)
+        return slugify(self.name)
 
     def __str__(self):
         return self.name
--- store/urls.py
+++ store/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products.views import index
+import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
