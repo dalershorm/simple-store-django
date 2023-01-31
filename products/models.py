f17eb7fb57cf2d5f287e6a1953d8c2375a475a44
abdurahman <abdurahman@abdurahmans-MacBook-Pro.local>
2023-01-31 21:43:03 +0500

    add modes category and product

--- products/models.py
+++ products/models.py
@@ -9,8 +9,8 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     image = models.ImageField(upload_to='categories/%Y/%m/%d')
-    status = models.CharField(choices=STATUS_CHOICES, default='active')
-    show_in_home = models.CharField(choices=STATUS_CHOICES, default='active')
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='active')
+    show_in_home = models.CharField(max_length=100, choices=STATUS_CHOICES, default='active')
 
     def slug(self):
         return slugify(self.name)
@@ -27,10 +27,10 @@ class Product(models.Model):
 
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
-    price = models.DecimalField()
-    status = models.CharField(choices=STATUS_CHOICES, default='active')
+    price = models.DecimalField(max_digits=10000, decimal_places=0)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='active')
     image = models.ImageField(upload_to='products/%Y/%m/%d')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def slug(self):
--- store/urls.py
+++ store/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products.views import index
-import settings
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
