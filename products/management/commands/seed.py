1dfeb6a259a3a7805701ec9edba861f6f026d933
abdurahman <abdurahman@abdurahmans-MacBook-Pro.local>
2023-01-31 22:26:57 +0500

    add modes category and product

--- products/management/commands/seed.py
+++ products/management/commands/seed.py
@@ -1,17 +1,31 @@
 from django.core.management.base import BaseCommand
 from products.models import Category
+import requests
 
 class Command(BaseCommand):
     help = 'Seeding Categories'
 
     def handle(self, *args, **options):
-        
-        category_list = [
-            'Мужской одежды',
-            'Ноутбуки',
-            'Телевизоры'
-        ]
 
-        for item in category_list:
-            Category.objects.create(name=item)
-            print(f"Добавлен категория '{item}'")
+        r = requests.get('https://qxtl5byxyj.execute-api.eu-central-1.amazonaws.com/prod/service_setting/widgets?city_id=1&widget_types=menu')
+        data = r.json()
+        print(data['response'][0]['widget_items'])
+        
+        # category_list = [
+        #     'Мужской одежды',
+        #     'Ноутбуки',
+        #     'Телевизоры'
+        # ]
+
+        for item in data['response'][0]['widget_items']:
+            Category.objects.create(name=item['label'])
+            print(f"Добавлен категория '{item['label']}'")
+
+            if len(item['child']) > 0:
+                for sun_item in item['child']:
+                    Category.objects.create(name=sun_item['label'])
+                    print(f"Добавлен категория '{sun_item['label']}'")
+
+
+
+
