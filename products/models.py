06005d22b3c12bcfe4de241c0274b4e846b079a4
abdurahman <abdurahman@abdurahmans-MacBook-Pro.local>
2023-01-31 21:55:51 +0500

    add modes category and product

--- products/models.py
+++ products/models.py
@@ -28,10 +28,12 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=10000, decimal_places=0)
+    description = models.TextField()
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='active')
     image = models.ImageField(upload_to='products/%Y/%m/%d')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def slug(self):
         return slugify(self.name)
