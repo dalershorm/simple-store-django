7c9247c54596f485ac4bceee84978af960c628e2
abdurahman <abdurahman@abdurahmans-MacBook-Pro.local>
2023-01-31 21:14:06 +0500

    add modes category and product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Category(models.Model):
+    STATUS_CHOICES = (
+        ("Актиный", "active"),
+        ("Не актиный", "deactive"),
+    )
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    image = models.ImageField(upload_to='categories/%Y/%m/%d')
+    status = models.CharField(choices=STATUS_CHOICES, default='active')
+    show_in_home = models.CharField(choices=STATUS_CHOICES, default='active')
+
+    def slug(self):
+        return slugify(self.slug)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    STATUS_CHOICES = (
+        ("Актиный", "active"),
+        ("Не актиный", "deactive"),
+    )
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(default=1.0)
+    status = models.CharField(choices=STATUS_CHOICES, default='active')
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def slug(self):
+        return slugify(self.slug)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+
+
+
--- store/urls.py
+++ store/urls.py
@@ -20,4 +20,4 @@ from products.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', index, name='index')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
